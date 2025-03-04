00d0749d06267447459cf2cbf534c20c8fab12d1
José Yael Varela García <132051688+WhatsNewsYoyo@users.noreply.github.com>
2025-03-04 14:51:08 -0600

    Add files via upload

---
+++ Yael_Varela_A01645324/js/api.js
@@ -0,0 +1,26 @@
+﻿async function CatImage(){
+
+    imageContainer.innerHTML = "<p>Cargando!</p>"
+    const response = await fetch("https://cataas.com/cat");
+    const blob = await response.blob();
+
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(blob);
+
+    imageContainer.innerHTML = "";
+    imageContainer.appendChild(img);
+}
+
+
+async function CatGif(){
+
+    imageContainer.innerHTML = "<p>Cargando!</p>"
+    const response = await fetch("https://cataas.com/cat/gif");
+    const blob = await response.blob();
+
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(blob);
+
+    imageContainer.innerHTML = "";
+    imageContainer.appendChild(img);
+}
